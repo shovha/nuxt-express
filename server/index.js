bcb7bb93fb2e5e48324868820aa93117b6228280
Shovha Saha <shovha.trinarylab@gmail.com>
2019-11-05 10:00:53 +0600

    sequlize partially initialized

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const consola = require('consola')
 const bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
-var morgan = require('morgan')
-var path = require('path')
-var rfs = require('rotating-file-stream')
-const { Nuxt, Builder } = require('nuxt')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+const path = require('path')
+const rfs = require('rotating-file-stream')
+const {Nuxt, Builder} = require('nuxt')
 const app = express()
 
 // Import and Set Nuxt.js options
@@ -13,45 +13,46 @@ const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start() {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+    // Init Nuxt.js
+    const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+    const {host, port} = nuxt.options.server
 
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
+    // Build only in dev mode
+    if (config.dev) {
+        const builder = new Builder(nuxt)
+        await builder.build()
+    } else {
+        await nuxt.ready()
+    }
 
-  // parse application/json
-  app.use(bodyParser.json())
+    // parse application/json
+    app.use(bodyParser.json())
 
-  // parse cookies
-  app.use(cookieParser());
-  
-  // create a rotating write stream
-  let accessLogStream = rfs('access.log', {
-    interval: '1d', // rotate daily
-    path: path.join(__dirname, 'log')
-  })
-  // setup the logger
-  app.use(morgan('combined', { stream: accessLogStream }))
+    // parse cookies
+    app.use(cookieParser());
 
-  app.get('/api', (req, res, next) => {
-    res.json({ message: "Api working" })
-  })
+    // create a rotating write stream
+    let accessLogStream = rfs('access.log', {
+        interval: '1d', // rotate daily
+        path: path.join(__dirname, 'log')
+    })
+    // setup the logger
+    app.use(morgan('combined', {stream: accessLogStream}))
 
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
+    app.get('/api', (req, res, next) => {
+        res.json({message: "Api working"})
+    })
 
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+    // Give nuxt middleware to express
+    app.use(nuxt.render)
+
+    // Listen the server
+    app.listen(port, host)
+    consola.ready({
+        message: `Server listening on http://${host}:${port}`,
+        badge: true
+    })
 }
+
 start()
